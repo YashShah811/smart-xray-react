3e855f0163c23aae9948963349dc1323cf4c301f
YashShah811 <shahyash81196@gmail.com>
2022-04-17 11:27:31 +0530

    package.json updated

--- src/properties.js
+++ src/properties.js
@@ -1,2 +1,2 @@
-// export const server = 'https://smart-xray-node-server.herokuapp.com'
-export const server = 'http://localhost:8182'
+// export const server = 'http://localhost:8182'
+export const server = 'http://192.46.212.152:8182'
