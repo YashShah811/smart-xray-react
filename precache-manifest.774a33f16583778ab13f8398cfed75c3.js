db3684f2ec0ac0614abccba9f09f4b23ad271e48
YashShah811 <shahyash81196@gmail.com>
2022-04-17 11:42:58 +0530

    Updates

---
+++ precache-manifest.774a33f16583778ab13f8398cfed75c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df0cba8190c6b3249e45ac2e4d01dd83",
+    "url": "/smart-xray-react/index.html"
+  },
+  {
+    "revision": "d220006499eeac28870d",
+    "url": "/smart-xray-react/static/css/main.3b05e813.chunk.css"
+  },
+  {
+    "revision": "10c86d58c7989709564e",
+    "url": "/smart-xray-react/static/js/2.9c9c301b.chunk.js"
+  },
+  {
+    "revision": "bc1606c6db11c396205f31e2fe0b9d0c",
+    "url": "/smart-xray-react/static/js/2.9c9c301b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d220006499eeac28870d",
+    "url": "/smart-xray-react/static/js/main.129808d2.chunk.js"
+  },
+  {
+    "revision": "814c83a533d003374270",
+    "url": "/smart-xray-react/static/js/runtime-main.310eaa59.js"
+  }
+]);
--- src/properties.js
+++
@@ -1,2 +0,0 @@
-// export const server = 'http://localhost:8182'
-export const server = 'http://192.46.212.152:8182'
