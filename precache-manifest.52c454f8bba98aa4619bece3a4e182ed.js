42dd1e9db8a475b7a09e3ded5cf1a0c5920956db
YashShah811 <shahyash81196@gmail.com>
2022-04-19 14:05:01 +0530

    Updates

---
+++ precache-manifest.52c454f8bba98aa4619bece3a4e182ed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5bc3691938b0d903193bcedade20fca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5aa0a5d07663d86251d7",
+    "url": "/static/css/main.3b05e813.chunk.css"
+  },
+  {
+    "revision": "10c86d58c7989709564e",
+    "url": "/static/js/2.9c9c301b.chunk.js"
+  },
+  {
+    "revision": "bc1606c6db11c396205f31e2fe0b9d0c",
+    "url": "/static/js/2.9c9c301b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5aa0a5d07663d86251d7",
+    "url": "/static/js/main.1c69fd12.chunk.js"
+  },
+  {
+    "revision": "fdf1399c65de807c5ff7",
+    "url": "/static/js/runtime-main.67e4f55e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/smart-xray-react/precache-manifest.f7237855399b1fdefbae43c09bdc66b4.js"
+  "/precache-manifest.52c454f8bba98aa4619bece3a4e182ed.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/smart-xray-react/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
