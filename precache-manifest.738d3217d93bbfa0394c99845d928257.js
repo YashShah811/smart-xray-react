52990cfaed6a1b38afd49285f945c52338345295
YashShah811 <shahyash81196@gmail.com>
2022-08-02 01:21:22 +0530

    Updates

---
+++ precache-manifest.738d3217d93bbfa0394c99845d928257.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de4292ee7dac73b00989cd744373ccb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "340b8f2f6b80c1729c29",
+    "url": "/static/css/main.6e005908.chunk.css"
+  },
+  {
+    "revision": "920a3bc26a97151e255a",
+    "url": "/static/js/2.64ed37f7.chunk.js"
+  },
+  {
+    "revision": "53750c25ca4ba323ac3a82543a199179",
+    "url": "/static/js/2.64ed37f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "340b8f2f6b80c1729c29",
+    "url": "/static/js/main.46a2edbd.chunk.js"
+  },
+  {
+    "revision": "ed6f2a14d61d2b54b656",
+    "url": "/static/js/runtime-main.804312f0.js"
+  }
+]);
