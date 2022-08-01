41b177658afa3bdf65cbb9a41b09fdea6406bc84
YashShah811 <shahyash81196@gmail.com>
2022-08-01 23:14:40 +0530

    server address chnaged

--- src/properties.js
+++ src/properties.js
@@ -1,2 +1,2 @@
-export const server = 'http://localhost:8182'
-// export const server = 'https://xray1.deepnets.in'
+// export const server = 'http://localhost:8182'
+export const server = 'https://xray1.deepnets.in'
